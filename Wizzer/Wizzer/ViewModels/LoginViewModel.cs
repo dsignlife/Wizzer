346e2c8d8855fa453b4e978e12babb66774183df
Tomz Phoz <dsign-life@hotmail.com>
2018-03-08 22:24:49 +0100

    added angular support

--- Wizzer/Wizzer/ViewModels/LoginViewModel.cs
+++ Wizzer/Wizzer/ViewModels/LoginViewModel.cs
@@ -12,7 +12,6 @@ namespace Wizzer.ViewModels
         [Required]
         public string Username { get; set; }
         [Required]
-
         public string Password { get; set; }
         public  bool RememberMe { get; set; }
     }
